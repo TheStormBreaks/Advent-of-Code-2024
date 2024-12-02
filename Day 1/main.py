b172b3b8c3c1dd39e5d6fa6c368acdcea4443edc
Somya <you@example.com>
2024-12-02 16:28:53 +0530

    day1

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,5 +1,5 @@
-#Advent of Code 2024
-#Day 1
+# Advent of Code 2024
+# Day 1
 
 from collections import Counter
 
@@ -7,23 +7,23 @@ file = open("Day 1/input.txt", "r")
 lines = file.readlines()
 file.close()
 
-#List 1 for elements on left
-#List 3 for elements on right
+# List 1 for elements on left
+# List 3 for elements on right
 list1, list2 = [], []
 
-#Left column == List 1 
-#Right column == List 2
+# Left column == List 1 
+# Right column == List 2
 for line in lines:
     left, right = map(int, line.split())
     list1.append(left)
     list2.append(right)
 
-#Arrange in order
+# Arrange in order
 list1.sort()
 list2.sort()
 
-#Find the difference at each position. 
-#Since they are ordered, element at index 1 is the smallest number. 
+# Find the difference at each position. 
+# Since they are ordered, element at index 1 is the smallest number. 
 distance_at_each_index = []
 for a, b in zip(list1, list2):
     distance = abs(a - b)                        #distance
@@ -33,7 +33,7 @@ total_distance = sum(distance_at_each_index)     #sum of all the distances
 #print("Difference at each index of ordered pair is: ", distance_at_each_index)
 print("Total Distance: (sum of all distance at each index) ", total_distance)
 
-#Similarity Score
+# Similarity Score
 similarity_score = 0
 counts_righty = Counter(list2)
 
